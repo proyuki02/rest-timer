fd9547d07f9ac2ac199a4a064c0f5705b68f9c07
piisuke2 <piisuke2@gmail.com>
2017-10-20 01:34:51 +0900

    -

--- app/clock.js
+++ app/clock.js
@@ -1,45 +1,23 @@
-const { remote } = require("electron");
-const calc = remote.require("./calc");
+const { ipcRenderer } = require("electron");
 
-// ウィンドウを開く
-openWindow();
+// main.jsからの通知で起動
+ipcRenderer.on("clock", function(event, arg) {
+  updateDigitalClock(arg);
+});
 
-// 時計の描画処理をスタート
-clock();
-
-function openWindow() {
-  // ウィンドウのオブジェクトを取得
-  const win = remote.getCurrentWindow();
-
-  // ウィンドウ位置を復元
-  if (localStorage.getItem("windowPosition")) {
-    const pos = JSON.parse(localStorage.getItem("windowPosition"));
-    win.setPosition(pos[0], pos[1]);
-  }
-
-  // クローズ時にウィンドウ位置を保存
-  win.on("close", function() {
-    localStorage.setItem("windowPosition", JSON.stringify(win.getPosition()));
-  });
-}
-
-function clock() {
-  // デジタル時計を更新
-  updateDigitalClock();
-
-  // 次の「0ミリ秒」に実行されるよう、次の描画処理を予約
-  const delay = 1000 - new Date().getMilliseconds();
-  setTimeout(clock, delay);
-}
-
-// 0埋め
 function fillZero(num) {
   return ("00" + num).slice(-2);
 }
 
-function updateDigitalClock() {
-  const rest = calc.getRest();
+function enableDrag(flag) {
+  if (flag) {
+    document.body.style["-webkit-app-region"] = "drag";
+  } else {
+    document.body.style["-webkit-app-region"] = "no-drag";
+  }
+}
 
+function updateDigitalClock(rest) {
   // 背景色
   if (rest < 1 * 60) {
     document.body.className = "alert";
@@ -51,12 +29,14 @@ function updateDigitalClock() {
   let text;
   if (rest < 0) {
     text = "TIME OVER";
+    enableDrag(false);
   } else {
     const restDate = new Date(rest * 1000);
     const hh = fillZero(restDate.getUTCHours());
     const mm = fillZero(restDate.getUTCMinutes());
     const ss = fillZero(restDate.getUTCSeconds());
     text = `REST ${hh}:${mm}:${ss}`;
+    enableDrag(true);
   }
   document.getElementById("digital_clock").innerHTML = text;
 }
--- app/main.js
+++ app/main.js
@@ -1,29 +1,33 @@
 const electron = require("electron");
 const { app, BrowserWindow, Menu, Tray, nativeImage, remote } = electron;
-const calc = require("./calc");
+const storage = require('electron-json-storage');
 let mainWindow = null;
 let trayIcon = null;
 let fullFlag = false;
 let showFlag = false;
 
-function clock() {
-  const rest = calc.getRest();
+function getRest() {
+  const now = new Date();
 
-  // 残り3分になったら
-  if (rest < 3 * 60 && !showFlag) {
-    show();
-    mainWindow.setAlwaysOnTop(true);
-    showFlag = true;
-  }
-  // 時間切れになったら
-  if (rest < 0 && !fullFlag) {
-    full();
-    fullFlag = true;
+  let hh, mm;
+  if (now.getHours() < 12) {
+    hh = 2;
+    mm = 30;
+  } else {
+    hh = 22;
+    mm = 30;
   }
 
-  // 次の「0ミリ秒」に実行されるよう、次の描画処理を予約
-  var delay = 1000 - new Date().getMilliseconds();
-  setTimeout(clock, delay);
+  const restMSec =
+    new Date(
+      now.getFullYear(),
+      now.getMonth(),
+      now.getDate(),
+      hh,
+      mm
+    ).getTime() - now.getTime();
+  const restMin = restMSec / 1000;
+  return restMin;
 }
 
 function show() {
@@ -40,7 +44,33 @@ function full() {
 function reset() {
   fullFlag = false;
   showFlag = false;
+  mainWindow.unmaximize();
   mainWindow.setAlwaysOnTop(false);
+}
+
+function clock() {
+  if (mainWindow !== null) {
+    const rest = getRest();
+
+    // 残り3分になったら
+    if (rest < 3 * 60 && !showFlag) {
+      show();
+      mainWindow.setAlwaysOnTop(true);
+      showFlag = true;
+    }
+    // 時間切れになったら
+    if (rest < 0 && !fullFlag) {
+      full();
+      fullFlag = true;
+    }
+
+    // 画面側に通知
+    mainWindow.webContents.send("clock", rest);
+  }
+
+  // 次の「0ミリ秒」に実行されるよう、次の描画処理を予約
+  var delay = 1000 - new Date().getMilliseconds();
+  setTimeout(clock, delay);
 }
 
 app.on("window-all-closed", function() {
@@ -52,8 +82,8 @@ app.on("window-all-closed", function() {
 app.on("ready", function() {
   mainWindow = new BrowserWindow({
     // ウィンドウ作成時のオプション
-    width: 180,
-    height: 50,
+    width: 1800,
+    height: 500,
     // ウィンドウの背景を透過
     transparent: true,
     // 枠の無いウィンドウ
@@ -63,14 +93,28 @@ app.on("ready", function() {
     // タスクバーに表示しない
     skipTaskbar: true,
     // アプリ起動時にウィンドウを表示しない
-    show: false
+    // show: false
+  });
+  mainWindow.on("closed", function() {
+    mainWindow = null;
   });
 
   // index.html を開く
   mainWindow.loadURL("file://" + __dirname + "/index.html");
 
-  mainWindow.on("closed", function() {
-    mainWindow = null;
+  // ウィンドウ位置を復元
+  storage.get('config.json', function(error, data) {
+    console.log(data);
+    if (!error && data.windowPosition) {
+      const pos = JSON.parse(data.windowPosition);
+      mainWindow.setPosition(pos[0], pos[1]);
+    }
+  });
+  mainWindow.on("close", function() {
+    const data = {
+      windowPosition: JSON.stringify(mainWindow.getPosition())
+    };
+    storage.set("config.json", data, function(error) {});
   });
 
   // タスクトレイに格納
@@ -105,14 +149,15 @@ app.on("ready", function() {
   ]);
   trayIcon.setContextMenu(contextMenu);
 
-  // タスクトレイのツールチップをアプリ名に
+  // タスクトレイのツールチップ
   trayIcon.setToolTip("Rest Timer");
 
-  // タスクトレイクリックでウィンドウをアクティブ
+  // タスクトレイクリック
   trayIcon.on("click", function() {
     show();
   });
-  // タスクトレイダブルクリックでリセット
+
+  // タスクトレイダブルクリック
   trayIcon.on("double-click", function() {
     reset();
   });
